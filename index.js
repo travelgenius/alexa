e4c6f39c684f9c2eb493cd48587629cd6fb49bba
Aristides Lourenco <aristidesfl@gmail.com>
2016-03-06 10:50:47 +0100

    Add functionality, still testing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var superagent = require('superagent');
+var moment = require('moment');
 /**
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
  * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
@@ -12,7 +13,7 @@ var superagent = require('superagent');
 // etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = function(event, context) {
   try {
-    console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+    // console.log("event.session.application.applicationId=" + event.session.application.applicationId);
 
     /**
      * Uncomment this if statement and populate with your skill's application ID to
@@ -87,6 +88,10 @@ function onIntent(intentRequest, session, callback) {
     getWelcomeResponse(callback);
   } else if ("SearchEventsByType" === intentName) {
     getEventsByType(intent, session, callback);
+  } else if ("SelectEvent" === intentName) {
+    selectEvent(intent, session, callback);
+  } else if ("NotInterested" === intentName) {
+    endSession(intent, session, callback);
   } else {
     throw "Invalid intent";
   }
@@ -106,10 +111,11 @@ function onSessionEnded(sessionEndedRequest, session) {
 
 function getWelcomeResponse(callback) {
   // If we wanted to initialize the session to have some attributes we could add those here.
-  var sessionAttributes = {};
+  var sessionAttributes = session.attributes || {};
   var cardTitle = "Welcome";
-  var speechOutput = "Welcome to Travel Genius. " +
-    "Please tell me which event you would like to attend. If you didn't decide on a specific event yet, ask me which events are available at a certain time.";
+  var speechOutput = //"Welcome to Travel Genius. " +
+    "Please tell me where you would like to go.";
+  // + " If you didn't decide on a specific event yet, ask me which events are available at a certain time.";
   // If the user either does not reply to the welcome message or says something that is not
   // understood, they will be prompted again with this text.
   var repromptText = "Please tell me which kind of event you would like to attend by saying, " +
@@ -121,16 +127,10 @@ function getWelcomeResponse(callback) {
 }
 
 
-function createFavoriteColorAttributes(favoriteColor) {
-  return {
-    favoriteColor: favoriteColor
-  };
-}
-
 function getColorFromSession(intent, session, callback) {
   var favoriteColor;
   var repromptText = null;
-  var sessionAttributes = {};
+  var sessionAttributes = session.attributes || {};
   var shouldEndSession = false;
   var speechOutput = "";
 
@@ -153,63 +153,104 @@ function getColorFromSession(intent, session, callback) {
     buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
 }
 
-function getEventsByType(intent, session, callback) {
-  var favoriteColor;
+function endSession(intent, session, callback) {
+  var sessionAttributes = session.attributes || {};
+  var speechOutput = "Have a good Hackatlon.";
   var repromptText = null;
-  var sessionAttributes = {};
+  var shouldEndSession = true;
+  console.log("==================== ENDING SESSION ==================")
+  callback(sessionAttributes,
+    buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+function getEventsByType(intent, session, callback) {
+  var repromptText = null;
+  var sessionAttributes = session.attributes || {};
   var shouldEndSession = false;
   var speechOutput = "";
-  var eventTypeSlot = intent.slots.EventType;
-  var eventDateSlot = intent.slots.DateType;
+  var eventType = intent.slots.EventType.value;
 
-  var monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-
-
-  if (eventTypeSlot) {
-    var eventType = eventTypeSlot.value;
-    sessionAttributes = { eventType: eventType };
-
-
-
-    superagent
-      .get('https://app.xapix.io/api/v1/thb16_travel_genius/events')
-      .set('Accept', 'application/json')
-      .set('Authorization-Token', 'GT4oMmLgAKk5HPBO2s6i9bawtuIYcDlp')
-      .query({ 'filter[type]': eventType })
-      .end(function(err, res) {
-        if (err || !res.ok) {
-          speechOutput = "I apologize, there was an error retrieving the events from the database.";
-          repromptText = "Please try again.";
-          callback(sessionAttributes,
-            buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-        } else {
-          var month = ''
-          var eventNames = res.body.map(function(event) {
-            month = monthNames[parseInt(event.startdate.split('.')[1])]
-            return event.title + ' in ' + month;
-          });
-
-          speechOutput = "I have found the following events, " + eventNames.join(', ');
-          repromptText = "Which one would you like to go to?";
-          callback(sessionAttributes,
-            buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-        }
-      });
-
-
-    // speechOutput = "I now know your favorite color is " + favoriteColor + ". You can ask me " +
-    // "your favorite color by saying, what's my favorite color?";
-
-  } else {
-    speechOutput = "I'm not sure what your favorite color is. Please try again";
-    repromptText = "I'm not sure what your favorite color is. You can tell me your " +
-      "favorite color by saying, my favorite color is red";
-    callback(sessionAttributes,
+  if (!eventType) {
+    speechOutput = "You have to specify at least an event type, like for example, Jazz or Techno.";
+    repromptText = null;
+    return callback(sessionAttributes,
       buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-
   }
+
+  sessionAttributes.eventType = eventType
+
+  superagent
+    .get('https://app.xapix.io/api/v1/thb16_travel_genius/events')
+    .set('Accept', 'application/json')
+    .set('Authorization-Token', 'GT4oMmLgAKk5HPBO2s6i9bawtuIYcDlp')
+    .query({ 'filter[type]': eventType })
+    .end(function(err, res) {
+      if (err || !res.ok) {
+        speechOutput = "I apologize, there was an error retrieving the events from the database.";
+        repromptText = "Please try again.";
+        return callback(sessionAttributes,
+          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+      }
+      var events = res.body;
+      var eventDate = intent.slots.EventDate.value || sessionAttributes.eventDate;
+      var eventLocation = intent.slots.EventLocation.value || sessionAttributes.eventLocation;
+
+      if (eventDate) {
+        console.log('======================================')
+        console.log("eventDate: ", eventDate)
+        console.log('======================================')
+        var eventDate = moment(eventDate);
+        sessionAttributes.eventDate = eventDate
+        events = events.filter(function(event) {
+          var eventStart = moment(event.startdate);
+          return eventStart.isBetween(eventDate) || eventStart.isSame(eventDate)
+        });
+      }
+
+      if (eventLocation) {
+        eventLocation = eventLocation.toLowerCase();
+        sessionAttributes.eventLocation = eventLocation;
+        events = events.filter(function(event) {
+          return eventLocation === event.city.toLowerCase() || eventLocation === event.country.toLowerCase()
+        });
+      }
+
+      // limit to 5
+      events = events.slice(0, 5)
+
+      var eventNames = events.map(function(event) {
+        return event.title; });
+
+
+
+
+
+      if (events.length === 0) {
+        speechOutput = "I didn't find any events matching that criteria. Please make a new search.";
+        repromptText = null;
+      } else {
+        speechOutput = "I have found " + events.length + " " + eventType + " events ";
+        if (eventLocation) {
+          speechOutput += ' in ' + eventLocation;
+        }
+        // if (eventDate) {
+        // speechOutput += ' ' + eventDate.from(moment());
+        // }
+        sessionAttributes.events = events;
+        speechOutput += ' : ' + eventNames.join(', ') + ". Does any of these events sound appealing to you?";
+        repromptText = "Would you like to know more about any of the events?";
+      }
+
+      callback(sessionAttributes,
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+
+    });
+
+
+  // speechOutput = "I now know your favorite color is " + favoriteColor + ". You can ask me " +
+  // "your favorite color by saying, what's my favorite color?";
+
+
 
   // Setting repromptText to null signifies that we do not want to reprompt the user.
   // If the user does not respond or says something that is not understood, the session
@@ -218,6 +259,50 @@ function getEventsByType(intent, session, callback) {
   // buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
 }
 
+function selectEvent(intent, session, callback) {
+  var repromptText = null;
+  var sessionAttributes = session.attributes || {};
+  var shouldEndSession = false;
+  var speechOutput = "";
+
+  var eventName = intent.slots.EventName.value;
+
+  var findEventByName = function(name, events) {
+    name = name.toLowerCase();
+    events.forEach(function(event) {
+      if (name === event.name.toLowerCase()) {
+        speechOutput = event.name + ' starts on ' + moment().calendar(event.date) + ' at ' + event.location;
+      }
+    });
+
+    speechOutput = speechOutput || "I didn't find any event named " + name + ". Please try again."
+    callback(sessionAttributes,
+      buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+  }
+  if (!eventName) {
+    var speechOutput = "I didn't get the name of the event you would like to know more about.";
+    return callback(sessionAttributes,
+      buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+
+  }
+  superagent
+    .get('https://app.xapix.io/api/v1/thb16_travel_genius/events')
+    .set('Accept', 'application/json')
+    .set('Authorization-Token', 'GT4oMmLgAKk5HPBO2s6i9bawtuIYcDlp')
+    // .query({ 'filter[name]': eventName })
+    .end(function(err, res) {
+      if (err || !res.ok) {
+        speechOutput = "I apologize, there was an error retrieving the events from the database.";
+        repromptText = "Please try again.";
+        callback(sessionAttributes,
+          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+      } else {
+        var events = res.body
+        findEventByName(eventName, events)
+      }
+    });
+
+}
 
 // --------------- Helpers that build all of the responses -----------------------
 
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 superagent = require('superagent')
-
+moment = require('moment')
 
 var monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
@@ -10,18 +10,25 @@ superagent
   .get('https://app.xapix.io/api/v1/thb16_travel_genius/events')
   .set('Accept', 'application/json')
   .set('Authorization-Token', 'GT4oMmLgAKk5HPBO2s6i9bawtuIYcDlp')
-  .query({'filter[type]': 'jazz'})
+  // .query({'filter[type]': 'jazz'})
   .end(function(err, res){
     if (err || !res.ok) {
       console.log('Oh no! error');
       console.log(err);
     } else {
-      var month = ''
-      var eventNames = res.body.map(function (event) {
-        month = monthNames[parseInt(event.startdate.split('.')[1])]
-        return event.title + ' in ' + month;
-      });
-      eventNames.join(', ')
-      console.log(eventNames)
+
+      res.body.forEach(function(event) {
+
+        if ('berlin' === event.city.toLowerCase() || 'berlin' === event.country.toLowerCase()) {
+          console.log(event.city);
+        }
+
+      })
+      // var eventNames = res.body.map(function (event) {
+      //   month = monthNames[parseInt(event.startdate.split('.')[1])]
+      //   return event.title;
+      // });
+      // eventNames.join(', ')
+      // console.log(eventNames)
     }
 });
